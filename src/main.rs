039a39e1d19259e89b9a9ef87fb11c4b0b7d2987
Alesha314 <alex.baltr@gmail.com>
2024-10-29 15:59:09 +0100

    fps, moving red left and right

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use crossterm::{
-    cursor, event::{Event, KeyCode, KeyEventKind}, execute, style::Print, terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen}, ExecutableCommand, QueueableCommand
+    cursor, event::{Event, KeyCode, KeyEventKind},
+    execute, 
+    style::Print, 
+    terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen}, 
+    ExecutableCommand, QueueableCommand
 };
-use std::io::{stdout, Write};
+use std::{io::{stdout, Write}, vec};
+use std::time::{Duration, Instant};
 
- fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     enable_raw_mode()?;
     execute!(stdout(), EnterAlternateScreen)?;
 
@@ -16,45 +21,58 @@ use std::io::{stdout, Write};
     grid[3][3] = 2;
     print_grid(grid.clone());
 
+    let mut last_time = Instant::now();
+
     loop {
-        let event = crossterm::event::read()?;
-        // println!("Event: {:?}", event);
-        if let Event::Key(key_event) = event {
-            // println!("Key pressed: {:?}", key_event);
-            if key_event.kind == KeyEventKind::Press {
-                if key_event.code == KeyCode::Char('a') || key_event.code == KeyCode::Char('A') {
-                    stdout.queue(Print("left\n"))?;
-                }
-                else if key_event.code == KeyCode::Char('d') || key_event.code == KeyCode::Char('D') {
-                    stdout.queue(Print("right\n"))?;
-                }
+        let now = Instant::now();
+        let elapsed = now.duration_since(last_time);
 
-                if key_event.code == KeyCode::Enter{
-                    stdout.queue(Print("enter\n"))?;
-                }
+        if elapsed >= Duration::from_millis(33) {
+            // clear all
+            stdout.queue(Clear(ClearType::All))?;
+            stdout.queue(cursor::MoveTo(0,0))?;
 
-                if key_event.code == KeyCode::Char('s') || key_event.code == KeyCode::Char('S') || key_event.code == KeyCode::Down{
-                    stdout.queue(Print("down\n"))?;
-                }
+            // edit row if needed
+            let event = crossterm::event::read()?;
+            // println!("Event: {:?}", event);
+            if let Event::Key(key_event) = event {
+                // println!("Key pressed: {:?}", key_event);
+                if key_event.kind == KeyEventKind::Press {
+                    if key_event.code == KeyCode::Char('a') || key_event.code == KeyCode::Char('A') {
+                        // stdout.queue(Print("left\n"))?;
+                        grid = move_red(grid.clone(), 2);
+                    }
+                    else if key_event.code == KeyCode::Char('d') || key_event.code == KeyCode::Char('D') {
+                        // stdout.queue(Print("right\n"))?;
+                        grid = move_red(grid.clone(), 0);
+                    }
 
-                if key_event.code == KeyCode::Left {
-                    stdout.queue(Print("arrow left\n"))?;
-                }
-                else if key_event.code == KeyCode::Right {
-                    stdout.queue(Print("arow right\n"))?;
-                }
-                if key_event.code == KeyCode::Backspace {
-                    stdout.queue(Clear(ClearType::All))?;
-                    stdout.queue(cursor::MoveTo(0,0))?;
-                    print_grid(grid.clone());
-                }
-            }           
-        } //🔳⬜🟥
-        stdout.flush()?;
+                    if key_event.code == KeyCode::Enter{
+                        // stdout.queue(Print("enter\n"))?;
+                    }
 
-        if event == Event::Key(KeyCode::Esc.into()) {
-            break;
-        }
+                    if key_event.code == KeyCode::Char('s') || key_event.code == KeyCode::Char('S') || key_event.code == KeyCode::Down{
+                        // stdout.queue(Print("down\n"))?;
+                    }
+
+                    if key_event.code == KeyCode::Left {
+                        // stdout.queue(Print("arrow left\n"))?;
+                    }
+                    else if key_event.code == KeyCode::Right {
+                        // stdout.queue(Print("arow right\n"))?;
+                    }
+                }           
+            } //🔳⬜🟥
+
+            print_grid(grid.clone());
+            stdout.flush()?;
+
+            if event == Event::Key(KeyCode::Esc.into()) {
+                break;
+            }
+
+            last_time = now;
+        }        
     }
 
     stdout.execute(LeaveAlternateScreen)?;
@@ -77,4 +95,33 @@ fn print_grid(grid: Vec<Vec<i8>>) {
         }
         println!();
     }
+}
+
+fn move_red(grid: Vec<Vec<i8>>, direction: i8) -> Vec<Vec<i8>> { //direction == 0: right, 1 - up, 2 - left, 3 - down
+    let mut new_grid = vec![];
+    if direction == 0 {
+        for row in grid {
+            let mut new_row = row.clone();
+            for (index, value) in row.iter().enumerate() {
+                if *value == 2 && index != 9 {
+                    new_row[index + 1] = 2;
+                    new_row[index] = 0;
+                }
+            }
+            new_grid.push(new_row);
+        }
+    }
+    else if direction == 2 {
+        for row in grid {
+            let mut new_row = row.clone();
+            for (index, value) in row.iter().enumerate() {
+                if *value == 2 && index != 0 {
+                    new_row[index - 1] = 2;
+                    new_row[index] = 0;
+                }
+            }
+            new_grid.push(new_row);
+        }
+    }
+    return new_grid;
 }
